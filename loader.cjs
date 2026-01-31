e2cb1a58a925215c91ec32fd85e5c67d1b346a83
Thilrash <ameenthilrash@gmail.com>
2026-01-31 23:06:21 +0530

    Removed loader js

--- loader.cjs
+++
@@ -1,5 +0,0 @@
-async function loadApp() {
-    // This path must match the physical location of your server file
-    await import('./dist/binarycraftsystems-web-app/server/server.mjs');
-}
-loadApp();
